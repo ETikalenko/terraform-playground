799461229f5cad6b12cef05997b8d1d1980eb55d
Elena-TikalenkoCDX <elena.tikalenko@countdown.co.nz>
2022-02-07 17:30:36 +1300

    uncomment appengine

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,15 +25,15 @@ module "enable_apis" {
   create_cmd_entrypoint  = "sh ${path.module}/scripts/gcloud.sh enable_apis"
   use_tf_google_credentials_env_var = true
 }
-//
-//resource "google_app_engine_application" "inbound-mail-app" {
-//  project = var.project_id
-//  location_id = var.location
-//
-//  depends_on = [
-//    module.gcloud,
-//  ]
-//}
+
+resource "google_app_engine_application" "inbound-mail-app" {
+  project = var.project_id
+  location_id = var.location
+
+  depends_on = [
+    module.enable_apis,
+  ]
+}
 
 module "deploy_appengine" {
   source  = "terraform-google-modules/gcloud/google"
@@ -47,27 +47,26 @@ module "deploy_appengine" {
   module_depends_on = [module.enable_apis]
 }
 
-//
-//resource "google_app_engine_firewall_rule" "gmail_traffic" {
-//  description  = "Only mail traffic is allowed"
-//  project      = var.project_id
-//  priority     = 1
-//  action       = "ALLOW"
-//  source_range = var.gmail_traffic_range
-//
-//  depends_on = [
-//    google_app_engine_application.inbound-mail-app
-//  ]
-//}
-//
-//resource "google_app_engine_firewall_rule" "default_rule" {
-//  description  = "Deny all traffic except gmail service"
-//  project      = var.project_id
-//  priority     = 2
-//  action       = "DENY"
-//  source_range = "*"
-//
-//  depends_on = [
-//    google_app_engine_application.inbound-mail-app
-//  ]
-//}
+resource "google_app_engine_firewall_rule" "gmail_traffic" {
+  description  = "Only mail traffic is allowed"
+  project      = var.project_id
+  priority     = 1
+  action       = "ALLOW"
+  source_range = var.gmail_traffic_range
+
+  depends_on = [
+    google_app_engine_application.inbound-mail-app
+  ]
+}
+
+resource "google_app_engine_firewall_rule" "default_rule" {
+  description  = "Deny all traffic except gmail service"
+  project      = var.project_id
+  priority     = 2
+  action       = "DENY"
+  source_range = "*"
+
+  depends_on = [
+    google_app_engine_application.inbound-mail-app
+  ]
+}
